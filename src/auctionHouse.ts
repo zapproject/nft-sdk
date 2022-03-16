9ef4e2319f7a104becc5d3392a2732ece3352cc6
Alex D <taejoonn@gmail.com>
2022-03-16 15:06:16 -0400

    Auction house documentation

--- src/auctionHouse.ts
+++ src/auctionHouse.ts
@@ -24,12 +24,22 @@ export interface Auction {
   auctionCurrency: string;
 }
 
+/**
+ * The Auction House class binding for the AuctionHouse smart contract. 
+ * An open auction house smart contract, enabling creators, collectors, and curators to sell their NFTs as their own auctions.
+ */
 class AuctionHouse {
   public readonly auctionHouse: Contract;
   public readonly chainId: number;
   public readonly signer: Signer;
   media: ZapMedia;
 
+  /**
+   * Constructor
+   * @param chainId - The network chain ID the auction house is associated with
+   * @param signer - The owner of the auction house
+   * @param customMediaAddress Optional argument if the auction house is for a custom media contract
+   */
   constructor(chainId: number, signer: Signer, customMediaAddress?: string) {
     this.chainId = chainId;
     this.signer = signer;
@@ -54,10 +64,20 @@ class AuctionHouse {
     }
   }
 
+  /**
+   * Getter for a auction object specified by the auction ID.
+   * @param auctionId - The auction ID identifier to retreive the auction object
+   * @returns The Promise auction object if it exists
+   */
   public async fetchAuction(auctionId: BigNumberish): Promise<any> {
     return await this.auctionHouse.auctions(auctionId);
   }
 
+  /**
+   * Getter for a auction object specified by the transaction receipt.
+   * @param receipt - The transaction receipt identifier to retreive the auction object
+   * @returns The Promise auction object if it exists
+   */
   public async fetchAuctionFromTransactionReceipt(
     receipt: TransactionReceipt
   ): Promise<Auction | null> {
@@ -73,6 +93,17 @@ class AuctionHouse {
     return null;
   }
 
+  /**
+   * Creates a new auction. Must be called by the owner or approved user of the specified token ID.
+   * @param tokenId - The token ID identifier the token being auctioned belongs to 
+   * @param tokenAddress - The media contract address associated with the token
+   * @param duration - The duration of the auction (in milliseconds)
+   * @param reservePrice - The reserve price for the auction
+   * @param curator - The optional curator address associated with the auction
+   * @param curatorFeePercentages - The optional curator fee percentage for the auction
+   * @param auctionCurrency - The currency associated with the auction
+   * @returns The Promise auction ID associated with the created auction
+   */
   public async createAuction(
     tokenId: BigNumberish,
     tokenAddress: string,
@@ -140,6 +171,12 @@ class AuctionHouse {
     }
   }
 
+  /**
+   * Approves the start of the specified auction, if a curator was set for the auction. Can only be called by the curator,
+   * @param auctionId - The auction ID identifier to approve the start of the auction
+   * @param approved - The approved flagged whether the start of the auction is approved or not
+   * @return The Promise of the contract call
+   */
   public async startAuction(auctionId: BigNumberish, approved: boolean) {
     // Fetches the auction details
     const auctionInfo = await this.auctionHouse.auctions(auctionId);
@@ -170,6 +207,12 @@ class AuctionHouse {
     return this.auctionHouse.startAuction(auctionId, approved);
   }
 
+  /**
+   * Sets the reserve price for the specified auction
+   * @param auctionId - The auction ID identifier to approve the start of the auction 
+   * @param reservePrice - The reserve price for the specified auction
+   * @returns The Promise of the contract call
+   */
   public async setAuctionReservePrice(
     auctionId: BigNumberish,
     reservePrice: BigNumberish
@@ -205,6 +248,13 @@ class AuctionHouse {
     }
   }
 
+  /**
+   * Creates a bid on the specified auction. Can only place a valid bid after the start of the auction.
+   * @param auctionId - The auction ID identifier to approve the start of the auction
+   * @param amount - The bid amount to be bid
+   * @param mediaContract - The media contract address associated with the auction
+   * @returns The Promise of the contract call
+   */
   public async createBid(
     auctionId: BigNumberish,
     amount: BigNumberish,
@@ -239,6 +289,12 @@ class AuctionHouse {
     }
   }
 
+  /**
+   * Cancels the specified auction. 
+   * Owner or curator can cancel the auction if the auction does not have a bid placed.
+   * @param auctionId - The auction ID identifier to approve the start of the auction
+   * @returns The Promise of the contract call
+   */
   public async cancelAuction(auctionId: BigNumberish) {
     const auctionInfo = await this.auctionHouse.auctions(auctionId);
 
@@ -269,6 +325,13 @@ class AuctionHouse {
     return this.auctionHouse.cancelAuction(auctionId);
   }
 
+  /**
+   * Ends the specified auction, only if called by owner or curator, a bid has been placed, 
+   * and the duration of the auction has passed
+   * @param auctionId - The auction ID identifier to approve the start of the auction
+   * @param mediaAddress - The media contract address associated with the auction
+   * @returns The Promise of the contract call
+   */
   public async endAuction(auctionId: BigNumberish, mediaAddress: string) {
     const auctionInfo = await this.auctionHouse.auctions(auctionId);
 
